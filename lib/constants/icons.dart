1afcc766af668653a1ee137b159caff491f5ecbe
Shashank-Girish <shashankgirish07@gmail.com>
2023-07-12 14:33:57 +0530

    Added icons const

---
+++ lib/constants/icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Map<String, IconData> iconsMap = {
+  'Home': Icons.home,
+  'Education': Icons.school,
+  'Food': Icons.fastfood,
+  'Shopping': Icons.shopping_bag,
+  'Transportation': Icons.directions_bus,
+  'Travel': Icons.flight,
+  'Health': Icons.medical_services,
+  'Entertainment': Icons.movie,
+  'Gift': Icons.card_giftcard,
+  'Miscellaneous': Icons.more_horiz,
+};
